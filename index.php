6678a86006fc80f5c230bc256365984a47fec8a8
Conley <cernst3@u.rochester.edu>
2018-01-28 16:23:05 -0500

    addition and skeleton of CSS
    basic introduction and implementation of CSS structure

--- index.php
+++ index.php
@@ -5,13 +5,28 @@
 		<title>Website</title>
 
 		<?php include('navigation.php');?>
+
+		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
-		<h1>Welcome</h1>
+		<h1>Conley Ernst</h1>
+
+		<img src="imgs/conley.jpg" alt="Conley Ernst">
 
 		<p>
 			<a href="school.php" target="_self">Link to page 2</a>
 		</p>
+
+		<div class="basicinfo">
+			<p>Majors: Computer Science and Digital Media Studies</p>
+			<p>Year: Class of 2019</p>
+		</div>
+
+		<div class="intro">
+			<p>
+				I am a Junior at the University of Rochester double majoring in Computer Science and Digital Media Studies, which provides the unique intersection of engineering and creative arts. Studying to be a software engineer, my academic and professional experience has given me a strong understanding of both the technical and user experience sides of a product.
+			</p>
+		</div>
 	</body>
 
 </html>
--- school.php
+++ school.php
@@ -17,6 +17,15 @@
 		<p>
 			<a href="index.php" target="_self">Link to HOME</a>
 		</p>
+
+		<p>
+			My academic career has had an interesting change of pace. I came in as a freshman with the intent of majoring in Digital Media Studies and Environmental Science. As a result, I spent my freshman year taking mainly Biology and Chemistry courses. The spring semester of my freshman year I took CSC 161 in order to fulfill a Digital Media Studies Requirement; this was my first introduction programming. Though I went in with the attitude that I was going to hate it, I ended up falling in love with programming and ended up switching my second major to Computer Science. I have since taken fundamental courses like Computation and Formal Systems in addition to courses like Mobile App Development and Web Programming. One of my favorite courses in Computer Science so far has been Video Game Design where I had the opportunity to apply my computer science skills to a new passion. Outside of computer science, I find myself drawn to art courses in order to complete my Digital Media Studies degree. One of my favorite courses has been New Media and Emerging Practices, were I got to continue my exploration of digital art. I have always had a passion for digital art and design, and am always looking for new was to combine this passion with my love for computer science. 
+		</p>
+
+		<p>
+			Outside of the classroom I am on the Varsity Lacrosse Team and hold a position on the executive board of my sorority. I’ve played lacrosse since I was in fourth grade and am goalie on the team here at UR. In my sorority I have held the executive board position as Panhellenic Delegate and currently serve as Risk Management chairwoman. Additionally, I have held executive council positions of Chapter Historian and Sunshine Chair. Lastly, I am currently a recruitment counselor (Rho Gamma) for the Panhellenic Association. 
+
+		</p>
 	</body>
 
 </html>
